0dc1ff4e67d9213db871415cdbc65d8ed1b360c6
MildaZakarkaite <zakarkaite.milda@gmail.com>
2019-10-22 17:33:12 +0300

    isaugo po viena eilute i table

--- classes/fileDB.php
+++ classes/fileDB.php
@@ -12,33 +12,38 @@ class FileDB {
     public function __load() {
         $this->data = file_to_array($this->file_name);
     }
-
+    
+    public function save() {
+        return array_to_file($this->data, $this->file_name);
+    }
+    
+    public function getData() {
+        return $this->data;
+    }
+    
     public function setData($data_array) {
         $this->data = $data_array;
     }
 
-    public function save() {
-        return array_to_file($this->data, $this->file_name);
-    }
-
-    public function getData() {
-        return $this->data;
-    }
-
     public function getRow($table, $row_id) {
-        if (isset($this->data[$table])) {
+//        if (isset($this->data[$table])) { Apsaugo jei toks neegzistuoja
             return $this->data[$table][$row_id];
-        }
+//        }
     }
 
-    public function addRow($table, $row, $row_id = null) {
-        if ($row_id !== null) {
-            $table;
-            $row;
-        } else {
-            $table;
-            $row;
-            $row_id;
-        }
-    }
+    public function addRow($table, $row) {
+        $this->data[$table][] = $row;
+}
+//public function addRow($table, $row, $row_id = null) {
+//        if ($row_id !== null) {
+//            $table;
+//            $row;
+//        } else {
+//            $table;
+//            $row;
+//            $row_id;
+//        }
+//    }
+//}
+//
 }
--- index.php
+++ index.php
@@ -3,6 +3,25 @@
 require 'classes/fileDB.php';
 require 'file.php';
 
-$db = new FileDB('failas.txt');
+$db = new FileDB('data/db.txt');
 
-$db -> save();
+$db->addRow('Vartotojai', ['name' => 'Milda', 'surname' => 'Zakarkaite']);
+$db->save();
+var_dump($db->getData());
+//
+//$db->setData(['table' => [['name' =>]]]);
+//
+//var_dump($db->getData());
+//
+//$db->getRow('table', 0);
+//
+//var_dump($db->getRow('table', 0));
+
+
+
+
+//$db->load();
+//$db->save();
+//Galima tureti kita objekta su kita duonbaze veikiancia tuo paciu principu
+//$db_2 = new FileDB('data/users.txt');
+
